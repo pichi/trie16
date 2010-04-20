eec62cac24c72682d96119364f9412d8e98fb347
Hynek Vychodil <vychodil.hynek@gmail.com>
2010-04-21 00:42:37 +0200

    Trie set and get functions.

--- src/trie16.erl
+++ src/trie16.erl
@@ -39,7 +39,64 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--export([map_set/3, map_get/2]).
+-export([new/0, set/3, get/2]).
+
+new() -> [].
+
+set(K, V, T) when is_binary(K) ->
+  set_(K, V, T).
+
+% trie tree contain nodes
+% {K, S, T} where K is bynary string, S is subtree, T value for key <<>> i.e. terminal
+% {M, T} where M is map contain subtrees as values, T is terminal as above
+% {[], T} as terminal node
+% [] - empty i.e. unset node
+set_(<<>>, V, []) -> term(V);
+set_(<<>>, V, {M, _}) -> {M, V};
+set_(<<>>, V, {K, S, _}) -> {K, S, V};
+set_(K, V, []) -> {K, term(V), []};
+set_(<<H:4, L:4, Rest/bytes>>, V, {M, V2}) ->
+  ST = set_(Rest, V, map_get(H, L, M)),
+  {map_set(H, L, ST, M), V2};
+set_(K, V, {K, ST, V2}) -> {K, set_(<<>>, V, ST), V2};
+set_(K, V, {K2, ST, V2}) ->
+  case prefix(K, K2, <<>>) of
+    {<<>>, K, K2} -> {splitmap(K, K2, V, ST), V2};
+    {P, <<>>, R2} -> {P, {R2, ST, V}, V2};
+    {P, R1, <<>>} -> {P, set_(R1, V, ST), V2};
+    {P, R1, R2} -> {P, {splitmap(R1, R2, V, ST), []}, V2}
+  end.
+
+-compile({inline, [term/1]}).
+
+term(V) -> {[], V}.
+
+prefix(<<A:8, R1/bytes>>, <<A:8, R2/bytes>>, P) ->
+  prefix(R1, R2, <<P/bytes, A:8>>);
+prefix(R1, R2, P) -> {P, R1, R2}.
+
+splitmap(<<H1:4, L1:4, R1/bytes>>, <<H2:4, L2:4, R2/bytes>>, V, ST) ->
+  M = map_set(H1, L1, set_(R1, V, []), []),
+  map_set(H2, L2, set2_(R2, ST), M).
+
+-compile({inline, [set2_/2]}).
+
+set2_(<<>>, ST) -> ST;
+set2_(K, ST) -> {K, ST, []}.
+
+get(K, T) when is_binary(K) ->
+  get_(K, T).
+
+get_(<<>>, {_, V}) -> V;
+get_(<<>>, {_, _, V}) -> V;
+get_(K, {K2, ST, _}) ->
+  S2 = byte_size(K2),
+  case K of
+    <<K2:S2/bytes, R/bytes>> -> get_(R, ST);
+    _ -> []
+  end;
+get_(<<H:4, L:4, R/bytes>>, {M, _}) -> get_(R, map_get(H, L, M));
+get_(_, []) -> [].
 
 map_set(I, V, A) ->
     map_set(I bsr 4, I band 15, V, A).
@@ -110,3 +167,6 @@ map_test_() ->
       map_keys(lists:foldl(
           fun(X, D) -> map_set(X, X, D) end,
           [], "Hello World!")))].
+
+trie_test_() ->
+  ?_assertEqual(bar, get(<<"foo">>, set(<<"foo">>, bar, new()))).
